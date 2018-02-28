29d8ce3774286abc9cac7b1c5399cb9f4a74cb12
yaolei <718229223@qq.com>
2018-02-28 10:48:05 +0800

    let's play hard

---
+++ 剑指offer/7.斐波那契数列/斐波那契数列.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int Fibonacci(int n) {
+        int preNum    = 1;
+        int prePreNum = 0;
+        int result    = 0;
+        if(n==0)
+        {
+            return 0;
+        }
+        if(n==1)
+        {
+            return 1;
+        }
+        for(int i=2; i<=n; i++)
+        {
+            result    = preNum + prePreNum;
+            prePreNum = preNum;
+            preNum    = result;
+        }
+        
+        return result;
+    }
+}
