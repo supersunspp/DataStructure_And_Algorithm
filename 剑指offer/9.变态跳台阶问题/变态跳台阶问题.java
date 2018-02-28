0b57c7a58430c2c277393e1149194af334709fde
yaolei <718229223@qq.com>
2018-02-28 11:26:09 +0800

    let's play hard

---
+++ 剑指offer/9.变态跳台阶问题/变态跳台阶问题.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int JumpFloorII(int target) {
+        if(target<=0)
+        {
+            return -1;
+        }
+        else if(target==1)
+        {
+            return 1;
+        }
+        else
+        {
+            return 2 * JumpFloorII(target - 1);
+        }
+    }
+}
