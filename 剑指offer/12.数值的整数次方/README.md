73f1ce99b47335d45c6e2bd0a4eaff0f9da75dc1
yaolei <718229223@qq.com>
2018-03-01 10:40:38 +0800

    let's play hard

---
+++ 剑指offer/12.数值的整数次方/README.md
@@ -0,0 +1,4 @@
+## 数值的整数次方
+给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+## 思路
+主要考虑exponent的边界条件。
---
+++ 剑指offer/12.数值的整数次方/数值的整数次方.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public double Power(double base, int exponent) {
+        if(exponent==0)
+        {
+            return 1;
+        }
+        else if(exponent>0)
+        {
+            double num = base;
+            for(int i=1; i<exponent; i++)
+            {
+                num = num * base;
+            }
+            
+            return num;
+        }
+        else
+        {
+            double nums = base;
+            int flag = -exponent;
+            for(int i=1; i<flag; i++)
+            {
+                nums = nums * base;
+            }
+            
+            return 1 / nums;
+        }
+  }
+}
