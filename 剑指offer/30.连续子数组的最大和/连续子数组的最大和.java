bd913e1d26da661371c197a8b51d42bcf93871f7
yaolei <718229223@qq.com>
2018-03-06 09:46:18 +0800

    let's play hard

---
+++ 剑指offer/30.连续子数组的最大和/连续子数组的最大和.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int FindGreatestSumOfSubArray(int[] array) {
+        if(array.length==0)
+            return 0;
+        
+        int total = array[0], maxSum = array[0];
+        for(int i=1; i<array.length; i++)
+        {
+            if(total>=0)
+            {
+                total += array[i];
+            }
+            else
+            {
+                total = array[i];
+            }
+            
+            if(total>maxSum)
+                maxSum = total;
+        }
+        
+        return maxSum;
+    }
+}
