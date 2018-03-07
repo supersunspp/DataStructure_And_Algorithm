3b9f6e295c7918eb86d4dbb9bcb2d9e8928284a1
yaolei <718229223@qq.com>
2018-03-07 15:59:17 +0800

    let's play hard

---
+++ 剑指offer/40.数组中只出现一次的数字/数组中只出现一次的数字.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
+        int length = array.length;
+        if(length==2)
+        {
+            num1[0] = array[0];
+            num2[0] = array[1];
+            return;
+        }
+        int bitResult = 0;
+        for(int i=0; i<length; ++i)
+            bitResult ^= array[i];
+        int index = findFirst1(bitResult);
+        
+        for(int i=0; i<length; ++i)
+        {
+            if(isBit1(array[i], index))
+                num1[0] ^= array[i];
+            else
+                num2[0] ^= array[i];
+        }
+    }
+    
+    public int findFirst1(int bitResult)
+    {
+        int index = 0;
+        while(((bitResult & 1)==0) && index<32)
+        {
+            bitResult >>= 1;
+            index++;
+        }
+        
+        return index;
+    }
+    
+    private boolean isBit1(int target, int index)
+    {
+        return ((target >> index) & 1) == 1;
+    }
+}
