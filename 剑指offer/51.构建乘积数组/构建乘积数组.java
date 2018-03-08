e05c4e5c8c5b66dd7bb334cd71d6053775ff5eed
yaolei <718229223@qq.com>
2018-03-08 17:46:13 +0800

    let's play hard

---
+++ 剑指offer/51.构建乘积数组/构建乘积数组.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class Solution {
+    public int[] multiply(int[] A) {
+        int length = A.length;
+        int[] B    = new int[length];
+        if(length!=0)
+        {
+            B[0] = 1;
+            for(int i=1; i<length; i++)
+                B[i] = B[i-1] * A[i-1];
+            
+            int temp = 1;
+            for(int j=length-2; j>=0; j--)
+            {
+                temp *= A[j+1];
+                B[j] *= temp;
+            }
+        }
+        
+        return B;
+    }
+}
