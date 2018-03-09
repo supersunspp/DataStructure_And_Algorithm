5893e9b979e0b623931207c4d14ca583ba2df962
yaolei <718229223@qq.com>
2018-03-09 14:45:12 +0800

    let's play hard

---
+++ 剑指offer/52.正则表达式匹配/正则表达式匹配.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public boolean match(char[] str, char[] pattern)
+    {
+        if(str==null || pattern==null)
+            return false;
+        
+        int strIndex     = 0;
+        int patternIndex = 0;
+        return matchCore(str, strIndex, pattern, patternIndex);
+    }
+    
+    public boolean matchCore(char[] str, int strIndex, char[] pattern, int patternIndex)
+    {
+        if(strIndex==str.length && patternIndex==pattern.length)
+            return true;
+        if(strIndex!=str.length && patternIndex==pattern.length)
+            return false;
+        
+        if(patternIndex+1<pattern.length && pattern[patternIndex+1]=='*')
+        {
+            if(strIndex!=str.length && pattern[patternIndex]==str[strIndex]
+              || strIndex!=str.length && pattern[patternIndex]=='.')
+            {
+                return matchCore(str, strIndex, pattern, patternIndex+2)
+                    || matchCore(str, strIndex + 1, pattern, patternIndex + 2)
+                    || matchCore(str, strIndex + 1, pattern, patternIndex);
+            }
+            else
+            {
+                return matchCore(str, strIndex, pattern, patternIndex + 2);
+            }
+        }
+        
+        if(strIndex!=str.length && pattern[patternIndex]==str[strIndex] 
+           || strIndex!=str.length && pattern[patternIndex]=='.')
+            return matchCore(str, strIndex+1, pattern, patternIndex+1);
+        
+        return false;
+    }
+}
