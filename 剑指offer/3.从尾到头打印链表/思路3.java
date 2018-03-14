817326edbd90b1f09f7a52fcd07e4f34cdbd86f3
yaolei <718229223@qq.com>
2018-03-14 20:34:42 +0800

    let's play hard

---
+++ 剑指offer/3.从尾到头打印链表/思路3.java
@@ -0,0 +1,27 @@
+/**
+*    public class ListNode {
+*        int val;
+*        ListNode next = null;
+*
+*        ListNode(int val) {
+*            this.val = val;
+*        }
+*    }
+*
+*/
+import java.util.ArrayList;
+public class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if(listNode==null)
+            return res;
+        
+        while(listNode!=null)
+        {
+            res.add(0, listNode.val);
+            listNode = listNode.next;
+        }
+        
+        return res;
+    }
+}
